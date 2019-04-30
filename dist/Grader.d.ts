4de8ce9dac3d9fefaf5e789a650265d9844748b9
Daniel Khan <daniel@khan.io>
2019-04-30 13:35:51 +0200

    remove dist

--- dist/Grader.d.ts
+++
@@ -1,7 +0,0 @@
-import * as pitometer from 'pitometer';
-export declare class Grader implements pitometer.IGrader {
-    grade(id: string, value: number | boolean, { thresholds, metricScore }: {
-        thresholds: any;
-        metricScore: any;
-    }, context: any): pitometer.IGradingResult;
-}
