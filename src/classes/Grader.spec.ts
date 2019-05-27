9207c41e12df639e609d143dd269cd065b72cefb
Daniel Khan <daniel@khan.io>
2019-05-27 08:31:53 +0200

    Improved handling of empty values and finalized tests

--- src/classes/Grader.spec.ts
+++ src/classes/Grader.spec.ts
@@ -17,6 +17,60 @@ const testUpperWarningSingle = {
   },
 };
 
+const testUpperWarningMultiple = {
+  id: 'Throughput_Booking',
+  results: [
+    {
+      key: 'SERVICE-84977E8DCF605164',
+      timestamp: 1557796260000,
+      value: 42,
+    },
+    {
+      key: 'SERVICE-84977E8DCF605164',
+      timestamp: 1557796260000,
+      value: 30,
+    },
+    {
+      key: 'SERVICE-84977E8DCF605164',
+      timestamp: 1557796260000,
+      value: 42,
+    }
+  ],
+  grading: {
+    type: 'Threshold',
+    thresholds: { upperSevere: 50, upperWarning: 40, lowerSevere: 10, lowerWarning: 20 },
+    metricScore: 10,
+    ignoreEmpty: false,
+  },
+};
+
+const testWarningSevereMix = {
+  id: 'Throughput_Booking',
+  results: [
+    {
+      key: 'SERVICE-84977E8DCF605164',
+      timestamp: 1557796260000,
+      value: 42,
+    },
+    {
+      key: 'SERVICE-84977E8DCF605164',
+      timestamp: 1557796260000,
+      value: 30,
+    },
+    {
+      key: 'SERVICE-84977E8DCF605164',
+      timestamp: 1557796260000,
+      value: 55,
+    },
+  ],
+  grading: {
+    type: 'Threshold',
+    thresholds: { upperSevere: 50, upperWarning: 40, lowerSevere: 10, lowerWarning: 20 },
+    metricScore: 10,
+    ignoreEmpty: false,
+  },
+};
+
 const testUpperSevereSingle = {
   id: 'Throughput_Booking',
   results: [{
@@ -69,6 +123,37 @@ const testPassSingle = {
     timestamp: 1557796260000,
     value: 30,
   }],
+  grading: {
+    type: 'Threshold',
+    thresholds: { upperSevere: 50, upperWarning: 40, lowerSevere: 10, lowerWarning: 20 },
+    metricScore: 20,
+    ignoreEmpty: false,
+  },
+};
+
+const testPassMultiple = {
+  id: 'Throughput_Booking',
+  results: [
+    {
+      key: 'SERVICE-84977E8DCF6051641',
+      timestamp: 1557796260000,
+      value: 30,
+    },
+    {
+      key: 'SERVICE-84977E8DCF6051642',
+      timestamp: 1557796260000,
+      value: 31,
+    },
+    {
+      key: 'SERVICE-84977E8DCF6051643',
+      timestamp: 1557796260000,
+      value: 32,
+    },
+    {
+      key: 'SERVICE-84977E8DCF6051644',
+      timestamp: 1557796260000,
+      value: 33,
+    }],
   grading: {
     type: 'Threshold',
     thresholds: { upperSevere: 50, upperWarning: 40, lowerSevere: 10, lowerWarning: 20 },
@@ -143,7 +228,7 @@ describe('ThresholdGrader', () => {
     expect(context).to.equal('Test');
   });
 
-  it('should detect annd grade upper warning on a single entity', async () => {
+  it('should detect and grade upper warning on a single entity', async () => {
     const test = testUpperWarningSingle;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
@@ -153,7 +238,17 @@ describe('ThresholdGrader', () => {
     expect(result.score).to.equal(test.grading.metricScore / 2);
   });
 
-  it('should detect annd grade upper severe breach on a single entity', async () => {
+  it('should detect and grade upper warning on multiple entities', async () => {
+    const test = testUpperWarningMultiple;
+    const grader = new Grader();
+    const result = grader.grade(test.id, test.results, test.grading);
+    expect(result.violations[0].breach).to.equal('upperWarning');
+    expect(result.violations[0].key).to.equal(test.results[0].key);
+    expect(result.violations[0].value).to.equal(test.results[0].value);
+    expect(result.score).to.equal(test.grading.metricScore / 2);
+  });
+
+  it('should detect and grade upper severe breach on a single entity', async () => {
     const test = testUpperSevereSingle;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
@@ -163,7 +258,22 @@ describe('ThresholdGrader', () => {
     expect(result.score).to.equal(0);
   });
 
-  it('should detect annd grade lower warning on a single entity', async () => {
+  it('should detect and grade an upper severe violation if there is also a warning', async () => {
+    const test = testWarningSevereMix;
+    const grader = new Grader();
+    const result = grader.grade(test.id, test.results, test.grading);
+    expect(result.violations.length).to.equal(2);
+    expect(result.violations[0].breach).to.equal('upperWarning');
+    expect(result.violations[0].key).to.equal(test.results[0].key);
+    expect(result.violations[0].value).to.equal(test.results[0].value);
+
+    expect(result.violations[1].breach).to.equal('upperSevere');
+    expect(result.violations[1].key).to.equal(test.results[2].key);
+    expect(result.violations[1].value).to.equal(test.results[2].value);
+    expect(result.score).to.equal(0);
+  });
+
+  it('should detect and grade lower warning on a single entity', async () => {
     const test = testLowerWarningeSingle;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
@@ -173,7 +283,7 @@ describe('ThresholdGrader', () => {
     expect(result.score).to.equal(test.grading.metricScore / 2);
   });
 
-  it('should detect annd grade upper severe breach on a single entity', async () => {
+  it('should detect and grade upper severe breach on a single entity', async () => {
     const test = testLowerSevereSingle;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
@@ -190,7 +300,7 @@ describe('ThresholdGrader', () => {
     expect(result.violations.length).to.equal(2);
   });
 
-  it('should report a violation if result is empty', async () => {
+  it('should report a violation if a single entity result is empty', async () => {
     const test = testEmptyResults;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
@@ -198,7 +308,8 @@ describe('ThresholdGrader', () => {
     expect(result.score).to.equal(0);
   });
 
-  it('should report a violation if result is empty and ignoreEmpty is true', async () => {
+  // tslint:disable-next-line: max-line-length
+  it('should report a violation if a single entity result is empty and ignoreEmpty is true', async () => {
     const test = testIgnoreEmptyResults;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
@@ -206,8 +317,16 @@ describe('ThresholdGrader', () => {
     expect(result.score).to.equal(test.grading.metricScore);
   });
 
-  it('should detect annd grade a single result with no violation', async () => {
+  it('should detect and grade a single result with no violation', async () => {
     const test = testPassSingle;
+    const grader = new Grader();
+    const result = grader.grade(test.id, test.results, test.grading);
+    expect(result.violations).to.be.empty;
+    expect(result.score).to.equal(test.grading.metricScore);
+  });
+
+  it('should detect and grade multiple results with no violation', async () => {
+    const test = testPassMultiple;
     const grader = new Grader();
     const result = grader.grade(test.id, test.results, test.grading);
     expect(result.violations).to.be.empty;
